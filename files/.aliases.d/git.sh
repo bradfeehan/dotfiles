b174687cce2db04ab45d158b067ed3cccdf6b881
Brad Feehan <git@bradfeehan.com>
2021-08-18 23:19:23 +1000

    Use my preferred oneline format for git log

--- files/.aliases.d/git.sh
+++ files/.aliases.d/git.sh
@@ -41,7 +41,7 @@ alias glgd='glg --date-order'
 #
 
 _git_log_medium_format='%C(bold)Commit:%C(reset) %C(green)%H%C(red)%d%n%C(bold)Author:%C(reset) %C(cyan)%an <%ae>%n%C(bold)Date:%C(reset)   %C(blue)%ai (%ar)%C(reset)%n%+B'
-_git_log_oneline_format='%C(green)%h%C(reset) %s%C(red)%d%C(reset)%n'
+_git_log_oneline_format='%C(yellow)%h%C(reset) %s%C(auto)%d%C(reset)'
 _git_log_brief_format='%C(green)%h%C(reset) %s%n%C(blue)(%ar by %an)%C(red)%d%C(reset)%n'
 
 # Git
