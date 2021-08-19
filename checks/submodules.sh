cb81dc1b86db4269ed560c9872ee3152b470a1c4
Brad Feehan <git@bradfeehan.com>
2021-08-19 23:51:56 +1000

    Move away from Zinit and plugin managers in general

---
+++ checks/submodules.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+set -euo pipefail
+IFS=$'\n\t'
+
+# The root directory of the dotfiles repository (containing this script)
+ROOT="$(cd "$(dirname "${BASH_SOURCE[0]}")/.." > /dev/null 2>&1 && pwd -P)"
+
+# Ensure submodule files are present
+while read -r file; do
+  submodule="$(cut -d/ -f 1-2 <<< "${file}")"
+  name="${file##${submodule}/}"
+  file="${ROOT}/submodules/${file}"
+
+  if [[ -f "${file}" ]]; then
+    debug "Found file '${name}' in submodule '${submodule}'."
+  else
+    echo "File '${name}' missing in submodule '${submodule}'."
+    echo 'If git submodules need to be initialised, try:'
+    echo
+    echo '  $ git submodule update --init --recursive'
+    exit 1
+  fi
+done < "${ROOT}/submodule-files"
--- checks/zinit.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-set -euo pipefail
-IFS=$'\n\t'
-
-check_git_remote "${HOME}/.zinit/bin" origin "git@github.com:zdharma/zinit.git"
