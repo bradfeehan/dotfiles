e832767858ce99a4a36ab1e3d9eba1de5d77b8d7
Brad Feehan <git@bradfeehan.com>
2023-02-07 02:17:13 +1100

    Skip vundle check if vim missing

--- checks/vundle.sh
+++ checks/vundle.sh
@@ -2,14 +2,18 @@
 set -euo pipefail
 IFS=$'\n\t'
 
-check_git_remote "${HOME}/.vim/bundle/Vundle.vim" origin \
-  "git@github.com:gmarik/Vundle.vim.git"
+if ! which vim > /dev/null 2>&1; then
+  debug 'Skipping vundle check because vim was not found in $PATH'
+else
+  check_git_remote "${HOME}/.vim/bundle/Vundle.vim" origin \
+    "git@github.com:gmarik/Vundle.vim.git"
 
-# Ensure Vundle has been installed itself
-if ! [[ -d "${HOME}/.vim/bundle/vundle/.git" ]]; then
-  echo "ERROR: Vundle Vim plugin isn't installed"
-  echo
-  echo "Try fixing it with:"
-  echo
-  echo "  vim +PluginInstall +qall"
+  # Ensure Vundle has been installed itself
+  if ! [[ -d "${HOME}/.vim/bundle/vundle/.git" ]]; then
+    echo "ERROR: Vundle Vim plugin isn't installed"
+    echo
+    echo "Try fixing it with:"
+    echo
+    echo "  vim +PluginInstall +qall"
+  fi
 fi
