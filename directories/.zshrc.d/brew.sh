10525ad9fb905bfe508044f4c876c285cf493cbf
Brad Feehan <brad.feehan@shopify.com>
2023-03-31 15:40:24 +1100

    Add brew find command to make installing unknown stuff easier

---
+++ directories/.zshrc.d/brew.sh
@@ -0,0 +1,18 @@
+brew () {
+  case "$1" in
+    find)
+      HOMEBREW_NO_COLOR=1 HOMEBREW_COLOR='' command brew search "$@" \
+        | sed '/^$/d' \
+        | xargs env HOMEBREW_NO_COLOR='' HOMEBREW_COLOR=1 brew desc --eval-all \
+        | sed 's/://;s/ / -- /' \
+        | fzf --ansi \
+                --preview "env HOMEBREW_NO_COLOR='' HOMEBREW_COLOR=1 brew info {1}" \
+                --preview-label='brew info' \
+        | cut -d' ' -f1 \
+        | xargs command brew install
+      ;;
+    *)
+      command brew "$@"
+      ;;
+  esac
+}
