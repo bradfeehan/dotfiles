42732d3f64fa77e914e1df386cccc4251953524f
Brad Feehan <brad.feehan@shopify.com>
2023-04-20 18:37:07 +1000

    Add aliases for Rubocop

--- directories/.zshrc.d/ruby.sh
+++ directories/.zshrc.d/ruby.sh
@@ -5,3 +5,7 @@ if [[ -f "${HOMEBREW_PREFIX:-/usr/local}/share/chruby/chruby.sh" ]]; then
     source "${HOMEBREW_PREFIX:-/usr/local}/share/chruby/auto.sh"
   fi
 fi
+
+alias cop='rubocop'
+alias bcop='bundle exec rubocop'
+alias becop='bundle exec rubocop'
