5d1769a19cb262017bef538fa7939feffbdcc813
Brad Feehan <git@bradfeehan.com>
2023-04-29 15:42:23 +1000

    Fix dash-man integration when python 2 is missing

--- directories/.zshrc.d/dash.sh
+++ directories/.zshrc.d/dash.sh
@@ -1,8 +1,29 @@
 # Use Dash for viewing man pages if present
 if [[ -x '/Applications/Dash.app/Contents/MacOS/Dash' ]]; then
   function man() {
-    local query="$*"
-    query="$(python -c "import urllib, sys; print urllib.quote(sys.argv[1])" "${query}")"
-    open "dash://man%3A${query}"
+    local query="$*" python='' uri=''
+
+    # Find python if present
+    if (( $+commands[python3] )); then
+      python='python3'
+    elif (( $+commands[python2] )); then
+      python='python2'
+    elif (( $+commands[python] )); then
+      python='python'
+    fi
+
+    # Create url-quoted string to pass to Dash
+    if [[ "${python}" == 'python3' ]]; then
+      uri="dash://man%3A$("${python}" -c "import urllib.parse, sys; print(urllib.parse.quote(sys.argv[1]))" "${query}")"
+    elif [[ "${python}" ]]; then
+      uri="dash://man%3A$("${python}" -c "import urllib, sys; print urllib.quote(sys.argv[1])" "${query}")"
+    fi
+
+    # Open dash if the URI was generated
+    if [[ "${uri}" ]]; then
+      open "${uri}"
+    else
+      command man "${query}"
+    fi
   }
 fi
