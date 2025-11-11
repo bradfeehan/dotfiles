ac400c5bb8386ad447cc92d4c9cddddc2211d778
Brad Feehan <git@bradfeehan.com>
2025-11-11 22:58:52 +1100

    Replace vscode CLI with cursor

--- directories/.zshrc.d/vscode.sh
+++ directories/.zshrc.d/vscode.sh
@@ -1,5 +1,1 @@
-if ! (( ${+commands[code]} )); then
-  if [[ -x '/Applications/Visual Studio Code.app/Contents/Resources/app/bin/code' ]]; then
-    alias code="'/Applications/Visual Studio Code.app/Contents/Resources/app/bin/code'"
-  fi
-fi
+alias code=cursor
